2179e20eaf192e52af65a178aa1abd5317cc7917
Nastasjia Plakhtii <n.plakhtii@gmail.com>
2018-09-04 15:44:08 +0300

    First commit

---
+++ src/components/Header/Header.component.ts
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({})
+export default class HeaderComponent extends Vue {
+
+  mounted (){
+    console.log('hello from app');
+  }
+  
+}
+
